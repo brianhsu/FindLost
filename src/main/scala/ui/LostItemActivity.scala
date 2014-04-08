26afb2d94837450b90bba1a6c0cecb71926ce013
BrianHsu <brianhsu.hsu@gmail.com>
2014-04-08 14:31:18 +0800

    Add star icon to action bar in lost item info activity.

--- src/main/scala/ui/LostItemActivity.scala
+++ src/main/scala/ui/LostItemActivity.scala
@@ -23,6 +23,20 @@ class LostItemActivity extends Activity with TypedViewHolder
   private lazy val dateTime = findView(TR.activityLostItemDateTime)
   private lazy val location = findView(TR.activityLostItemLocation)
   private lazy val description = findView(TR.activityLostItemDescription)
+  private var actionStar: MenuItem = _
+  private var isStar: Boolean = false
+
+  def toggleStar() {
+    isStar = !isStar
+    isStar match {
+      case true => actionStar.setIcon(R.drawable.ic_action_important_color)
+      case false => actionStar.setIcon(R.drawable.ic_action_important)
+    }
+  }
+
+  def onActionStarClicked(menuItem: MenuItem) {
+    toggleStar()
+  }
 
   def searchDepartment(view: View) {
     import android.net.Uri
@@ -32,6 +46,16 @@ class LostItemActivity extends Activity with TypedViewHolder
     if (intent.resolveActivity(getPackageManager()) != null) {
       startActivity(intent)
     }
+  }
+
+  override def onCreateOptionsMenu(menu: Menu): Boolean = {
+    val inflater = getMenuInflater
+    inflater.inflate(R.menu.activity_lost_item_actions, menu)
+    actionStar = menu.findItem(R.id.activityLostItemActionStar).asInstanceOf[MenuItem]
+    if (isStar) {
+      actionStar.setIcon(R.drawable.ic_action_important_color)
+    }
+    super.onCreateOptionsMenu(menu)
   }
 
   override def onCreate(savedInstanceState: Bundle)  {
